610b96eb75aebcc8d7a55d84bc6f6506b37e890d
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-13 10:30:32 -0500

    commit tree f2ba5a98abd4ecc821ee3992e69995d77070ea97

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-  }
+    return movieData.map((data) => (
+      <MovieCard
+        title={data.title}
+        IMDBRating={data.IMDBRating}
+        genres={data.genres}
+        poster={data.poster}
+      />
+    ));
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
+
+MovieShowcase.defaultProps = {
+  title: "Yehuda Title",
+  IMDBRating: 0,
+  genres: ["No Genres"],
+  poster: "No Poster",
+};
--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class CardFront extends Component {
-
   render() {
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
-      </div>
-    )
+      <div
+        className="card-front"
+        style={{ backgroundImage: `url(${this.props.poster})` }}
+      ></div>
+    );
   }
 }
+
+// CardFront.defaultPoster = {
+//   poster:
+//     "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+// };
